3b06fd3abe04b4770afca19ebb87e02b202bdff9
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2018-06-06 19:55:42 +0530

    Add bash script to crontab for backing up MongoDB

---
+++ mongobackup.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+#=====================================================================
+# Set the following variables as per your requirement
+#=====================================================================
+# Database Name to backup
+MONGO_DATABASE="mydb"
+# Database host name
+MONGO_HOST="127.0.0.1"
+# Database port
+MONGO_PORT="27017"
+# Backup directory
+BACKUPS_DIR="/var/backups/$MONGO_DATABASE"
+# Database user name
+DBUSERNAME="username"
+# Database password
+DBPASSWORD="passw0rd"
+# Authentication database name
+DBAUTHDB="admin"
+# Days to keep the backup
+DAYSTORETAINBACKUP="14"
+#=====================================================================
+
+TIMESTAMP=`date +%F-%H%M`
+BACKUP_NAME="$MONGO_DATABASE-$TIMESTAMP"
+
+echo "Performing backup of $MONGO_DATABASE"
+echo "--------------------------------------------"
+# Create backup directory
+if ! mkdir -p $BACKUPS_DIR; then
+  echo "Can't create backup directory in $BACKUPS_DIR. Go and fix it!" 1>&2
+  exit 1;
+fi;
+# Create dump
+mongodump -d $MONGO_DATABASE --username $DBUSERNAME --password $DBPASSWORD --authenticationDatabase $DBAUTHDB
+# Rename dump directory to backup name
+mv dump $BACKUP_NAME
+# Compress backup
+tar -zcvf $BACKUPS_DIR/$BACKUP_NAME.tgz $BACKUP_NAME
+# Delete uncompressed backup
+rm -rf $BACKUP_NAME
+# Delete backups older than retention period
+find $BACKUPS_DIR -type f -mtime +$DAYSTORETAINBACKUP -exec rm {} +
+echo "--------------------------------------------"
+echo "Database backup complete!"
+
+# chomd +x mongobackup.sh
+# crontab -e
+# MongoDB Backup
+# 00 00 * * * /path/to/script/mongobackup.sh
+# Thanks to -
+# https://pranavprakash.net/2017/02/04/automate-mongodb-backups-using-cron-and-shell-script/
+
+
+
+
+
+
+
+
+
+
